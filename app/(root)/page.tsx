c4d63f1f500dc7941b0dc2439b125650b340d3d7
AbdelrahmanM05 <Abdelrahmanabdelaal05@gmail.com>
2025-02-24 23:25:55 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import { Button } from "@/components/ui/button";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
+import RightSidebar from "@/components/RightSidebar";
 
-export default function Home() {
+const Home = () => {
+  const loggedIn = { firstName: "Abdo",lastName:"MH",email:"conect@abdomh.com" };
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "g"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactoins={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
+    </section>
   );
-}
+};
+
+export default Home;
